a9e73bbea46fd48a38e1a310e3722e285e5026ec
CommanderBubble <CommanderBubble@users.noreply.github.com>
2024-09-14 16:27:06 +1000

    blacklist entities from assembler-pipe-passthrough

---
+++ src/data-updates.lua
@@ -0,0 +1,7 @@
+if mods["assembler-pipe-passthrough"] then
+    appmod.blacklist["wsf-big-assembly"] = true
+    appmod.blacklist["wsf-big-centrifuge"] = true
+    appmod.blacklist["wsf-big-chemplant"] = true
+    appmod.blacklist["wsf-big-furnace"] = true
+    appmod.blacklist["wsf-big-refinery"] = true
+end
